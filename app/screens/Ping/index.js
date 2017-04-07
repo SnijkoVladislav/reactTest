d4d3628c820c47d6739a03dcc5c9255a4cfd20ce
Your Name <you@example.com>
2017-04-07 18:31:34 +0300

    fix ping

--- app/screens/Ping/index.js
+++ app/screens/Ping/index.js
@@ -3,59 +3,44 @@ import * as pingServices from './pingServices';
 import {Link} from 'react-router';
 
 export default class User extends Component {
-  constructor() {
-    super();
-    this.state = {
-      serverStatus: '',
-      serverResponceTime: '',
-      isPing: true
+    constructor() {
+        super();
+        this.state = {
+            serverStatus: '',
+            serverResponceTime: '',
+            isPing: false
+        }
     }
-  }
 
-  handleClick = () => {
-    this.setState({
-      isPing: !this.state.isPing
-    });
-  }
-
-  startPing = () => {
-    if (this.state.isPing) {
-      let timeStart = +Date.now();
-      pingServices.ping().then(() => {
-        this.setState({
-          serverStatus: 'success',
-          serverResponceTime: + Date.now() - timeStart
+    startPing = () => {
+        this.setState({isPing: true});
+        pingServices.ping(this.state.url).then(result => {
+            this.setState({serverStatus: "secuses", isPing: false, serverResponceTime: result});
+        }).catch((err) => {
+            this.setState({serverStatus: "error", isPing: false, serverResponceTime: ''});
         });
-        setTimeout(() => {
-          this.startPing();
-        }, 3000);
-      }, () => {
-        this.setState({serverStatus: 'error'});
-        setTimeout(() => {
-          this.startPing();
-        }, 9000);
-      })
     }
-  }
 
-  render() {
-    return (
-      <section className="container home">
-        <div>
-          <span>
-            <Link to="math"> math </Link>
-          </span>
-          <span>
-            <Link to="ping"> ping </Link>
-          </span>
-        </div>
-        <button onClick={this.startPing}>start</button>
-        <div>serverStatus : {this.state.serverStatus}</div>
-        <div>serverResponceTime: {this.state.serverResponceTime}</div>
-        <button onClick={this.handleClick}>pingStatus : {this.state.isPing
-            ? 'can ping'
-            : 'stop ping'}</button>
-      </section>
-    );
-  }
+    render() {
+        return (
+            <section className="container home">
+                <div>
+                    <span>
+                        <Link to="/">
+                            math
+                        </Link>
+                    </span>
+                    <span>
+                        <Link to="ping">
+                            ping
+                        </Link>
+                    </span>
+                </div>
+                <input type="text" onChange={e => this.state.url = e.target.value}/>
+                <button onClick={this.startPing} className={!(this.state.isPing) && 'btn-disable'}>ping</button>
+                <div>serverStatus : {this.state.serverStatus}</div>
+                <div>serverResponceTime: {this.state.serverResponceTime}</div>
+            </section>
+        );
+    }
 }
