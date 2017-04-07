8e1ee63fe61ae59141277990dd7d3a0a02f479e8
Your Name <you@example.com>
2017-04-07 18:35:54 +0300

    fix ping

--- app/screens/Ping/pingServices.js
+++ app/screens/Ping/pingServices.js
@@ -1,9 +1,29 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/fileForPing';
-
 export {ping};
 
-function ping() {
-  return axios.get(BASE_URL);
+function request_image(url) {
+    return new Promise((resolve, reject) => {
+        var img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = () => reject(url);
+        img.src = url + '?random-no-cache=' + Math.floor((1 + Math.random()) * 0x10000).toString(16);
+    });
+}
+
+function ping(url, multiplier) {
+    return axios.get(url).then(() => {
+        return new Promise((resolve, reject) => {
+            var start = (new Date()).getTime();
+            var response = () => {
+                var delta = ((new Date()).getTime() - start);
+                delta *= (multiplier || 1);
+                resolve(delta);
+            };
+            request_image(url).then(response).catch(response);
+            setTimeout(() => {
+                reject(Error('Timeout'));
+            }, 5000);
+        });
+    })
 }
